51a5de47183851c885643d28c6fa0597e163f857
Tomás Lara <tomas.lara@tnschile.com>
2023-11-20 12:11:33 -0300

    feat(template-mapping): Adds rest of params in Template models
    - Add counts params to Template for more info
    - Adds import flag in migration to check workflow

--- src/domain/model/ZabbixTemplate.go
+++ src/domain/model/ZabbixTemplate.go
@@ -4,10 +4,19 @@ import "gorm.io/gorm"
 
 type ZabbixTemplate struct {
 	gorm.Model
-	Templateid  string `json:"templateid" gorm:"type:varchar(255);not null"`
-	Name        string `json:"name" gorm:"not null"`
-	Host        string `json:"host" gorm:"not null"`
-	Description string `json:"description" gorm:"not null"`
+	Templateid  string            `json:"templateid" gorm:"type:varchar(255);not null"`
+	Name        string            `json:"name" gorm:"not null"`
+	Host        string            `json:"host" gorm:"not null"`
+	Description string            `json:"description" gorm:"not null"`
+	HostCount   int               `json:"hosts,string" gorm:"not null"`
+	Parents     []*ZabbixTemplate `json:"parentTemplates" gorm:"many2many:zabbix_parent_templates;"`
+	Items       int               `json:"items,string" gorm:"not null"`
+	Triggers    int               `json:"triggers,string" gorm:"not null"`
+	Graphs      int               `json:"graphs,string" gorm:"not null"`
+	Screens     int               `json:"screens,string" gorm:"not null"`
+	Discoveries int               `json:"discoveries,string" gorm:"not null"`
+	HttpTests   int               `json:"httpTests,string" gorm:"not null"`
+	Macros      int               `json:"macros,string" gorm:"not null"`
 
 	// Migration in which this proxy is mapped
 	MigrationID uint       `json:"migrationId" gorm:"not null"`
@@ -24,4 +33,9 @@ type ZabbixTemplateMapping struct {
 	SourceTemplate        *ZabbixTemplate `gorm:"foreignKey:SourceTemplateID"`
 	DestinationTemplateID uint            `json:"destinationTemplateId" gorm:"not null"`
 	DestinationTemplate   *ZabbixTemplate `gorm:"foreignKey:DestinationTemplateID"`
+}
+
+type ZabbixParentTemplate struct {
+	TemplateID uint   `json:"templateId,string" gorm:"not null;primaryKey;autoIncrement:false"`
+	Host       string `json:"host" gorm:"not null"`
 }
